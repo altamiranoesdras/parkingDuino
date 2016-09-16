787450feccdcdd9cf0c002ae1029de845964cdcb
Esdras Altamirano <alramiranoesdras@gmail.com>
2016-09-15 18:18:53 -0600

    Corregido error servo quedava bibrando en posicion 0

--- parkingDuino.ino
+++ parkingDuino.ino
@@ -1,25 +1,27 @@
 #include <Servo.h>
-
-Servo myservo;  // create servo object to control a servo
-
-int potpin = 0;  // analog pin used to connect the potentiometer
+Servo myservo;
 int boton=2;
-int sts=0;
-int val;    // variable to read the value from the analog pin
+int localLed=13;
+int sts=0;    
 
 void setup() {
-  pinMode(boton,OUTPUT);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-  myservo.write(0);                  // sets the servo position according to the scaled value
+  pinMode(boton,INPUT);
+  pinMode(localLed,OUTPUT);
+  myservo.attach(9,660,2545);  
+  myservo.write(0);   
 }
 
 void loop() {
   sts = digitalRead(boton);  // read input value
+  
   if (sts == HIGH) {
+    digitalWrite(localLed,HIGH);
      myservo.write(90); 
      delay(2000);
-     myservo.write(0); 
+     myservo.write(0);
+     //myservo.detach(9);
+  }else{
+    digitalWrite(localLed,LOW);
+     delay(20);
   }
-  
-  
 }
