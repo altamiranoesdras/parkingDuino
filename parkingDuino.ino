76ac3bad5cffc19180e3bf98ba8c99e640132cbc
Esdras Altamirano <alramiranoesdras@gmail.com>
2016-09-20 19:59:01 -0600

    Agregada LCD a codigo fuente para mostrar numero de plasas desocupadas

--- parkingDuino.ino
+++ parkingDuino.ino
@@ -1,27 +1,51 @@
 #include <Servo.h>
+#include<Wire.h>
+#include<LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F,16,2);
+
 Servo myservo;
 int boton=2;
 int localLed=13;
-int sts=0;    
+int sts=0;
+int contador=4;    
 
 void setup() {
   pinMode(boton,INPUT);
   pinMode(localLed,OUTPUT);
-  myservo.attach(9,660,2545);  
-  myservo.write(0);   
+  myservo.attach(9,650,2545);  
+  myservo.write(180);   
+
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Parqueos ");
+  lcd.setCursor(0,1);
+  lcd.print("Disponibles :");
+  lcd.setCursor(14,1);
+  lcd.print(contador);
 }
 
 void loop() {
-  sts = digitalRead(boton);  // read input value
+  
+   sts = digitalRead(boton);  // read input value
   
   if (sts == HIGH) {
-    digitalWrite(localLed,HIGH);
-     myservo.write(90); 
-     delay(2000);
-     myservo.write(0);
-     //myservo.detach(9);
+    if(contador>0){
+      digitalWrite(localLed,HIGH);
+      myservo.write(90); 
+      delay(2000);
+      myservo.write(180);
+      contador=contador-1;
+      lcd.clear(); 
+      lcd.print("Parqueos ");
+      lcd.setCursor(0,1);
+      lcd.print("Disponibles :");
+      lcd.setCursor(14,1);
+      lcd.print(contador);
+    }
   }else{
-    digitalWrite(localLed,LOW);
+     digitalWrite(localLed,LOW);
      delay(20);
   }
 }
